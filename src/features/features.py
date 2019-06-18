adb1ce6d6ea29d8fb50562ed369d03d110a04e8f
Mikhail <mnrozhkov@gmail.com>
2019-06-18 13:36:39 +0300

    Update notebooks

--- src/features/features.py
+++ src/features/features.py
@@ -7,7 +7,7 @@ def extract_features(df):
     dataset['sepal_length_to_sepal_width'] = dataset['sepal_length'] / dataset['sepal_width']
     dataset['petal_length_to_petal_width'] = dataset['petal_length'] / dataset['petal_width']
 
-    ## Uncomment for Experiments
+    ## Uncomment for Experiments 3
     # dataset['sepal_length_in_square'] = dataset['sepal_length'] ** 2
     # dataset['sepal_width_in_square'] = dataset['sepal_width'] ** 2
     # dataset['petal_length_in_square'] = dataset['petal_length'] ** 2
