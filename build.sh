5fe265d7ab2dbc2052a4f648c2eab5160429daf7
Alex <alexckolosov@gmail.com>
2020-10-20 00:29:02 +0900

    Update versions of Python libraries

--- build.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-export $(cat config/.env | grep "^[^#;]")
-docker build \
-        --build-arg GIT_CONFIG_USER_NAME=$GIT_CONFIG_USER_NAME  \
-        --build-arg GIT_CONFIG_EMAIL=$GIT_CONFIG_EMAIL \
-        -t dvc-2-iris-demo:latest .
